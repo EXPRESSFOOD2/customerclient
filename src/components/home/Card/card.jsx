843ad1e88f3c82faa56977c6c86e8f8e8b98e34e
ninak8 <103591333+ninak8@users.noreply.github.com>
2023-02-10 14:42:31 -0300

    agregando card

---
+++ src/components/home/Card/card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import style from "./card.module.css";
+
+const Card = (props) => {
+  return (
+    <div className={style.contentCard}>
+      <img
+        src="https://tn.com.ar/resizer/rLIhhhDVS3qw8WB0Ebke_-U16uk=/1440x0/smart/filters:format(webp)/cloudfront-us-east-1.images.arcpublishing.com/artear/77ST57RMRJBWDNLNCKVY5AKLD4.jpg"
+        alt="burguer"
+        className={style.image}
+      />
+
+      <div className={style.contentInfo}>
+        <div className={style.InfoLocal}>
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/010/252/103/non_2x/cute-cartoon-hamburger-file-free-png.png"
+            alt="local"
+            className={style.imageLocal}
+          />
+          <div className={style.info}>
+            <h3>Nombre </h3>
+            <h5>10-40 min</h5>
+          </div>
+        </div>
+        {/*  */}
+        <div className={style.km}>
+          <h5>4 km away</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
