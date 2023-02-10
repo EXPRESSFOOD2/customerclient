be1cd048a8d7bc8b564b5016f24f5f678aac7540
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-10 11:30:44 -0500

    adding navbar to home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "../../components/home/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+}
