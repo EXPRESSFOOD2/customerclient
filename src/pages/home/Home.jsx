964a43283cc54e0726f80b883141f59558bafc1e
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-18 13:17:46 -0500

    adding carousel in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import LandingPage from "../../components/home/landingPage/LandingPage";
+import Carousel from "../../components/home/carousel/Carousel";
 
 export default function Home() {
-
   return (
     <div>
+      <Carousel />
       <LandingPage />
     </div>
   );
