3002aeeda1cbe456985fb5be32f29f7ecc6cf0e0
constantin_m <cmitrokhin@applaudostudios.dev>
2023-02-19 06:20:24 -0300

    Armar un esqueleto para los filtros

--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -4,6 +4,8 @@ import ProductCard from "../../components/store/Product/ProductCard";
 import style from "./Store.module.css";
 import { NavLink } from "react-router-dom";
 import Pagination from "../../shared/Pagination/Pagination";
+import Filter from "../../shared/Filter/Filter"
+import FILTER_OPTIONS from "../../shared/Filter/Filter_Options"
 
 export default function Store() {
   const menu = useSelector((state) => state.fullMenu); // Hook de traer data del estado global
@@ -14,19 +16,20 @@ export default function Store() {
       </NavLink>
     );
   });
-
+  const { f1, f2, f3 } = FILTER_OPTIONS;
   const [pagina, setPagina] = useState(1);
   const [porPagina] = useState(9);
   let maximo = Math.ceil(menu.length / porPagina);
-
+  console.log("F1: "+f1.title);
   return (
     <>
       <Pagination pagina={pagina} setPagina={setPagina} maximo={maximo} />
       <div className={style.rows}>
-        {/* <div>
-          <p>opcion 1</p>
-          <p>opcion 2</p>
-        </div> */}
+        <div>
+          <Filter title={f1.title} options={f1.options} action={f1.action} />
+          <Filter title={f2.title} options={f2.options} action={f2.action} />
+          <Filter title={f3.title} options={f3.options} action={f3.action} />
+        </div>
         <div className={style.container}>
           {products.slice(((pagina-1)*porPagina), (pagina)*porPagina)}
         </div>
---
+++ src/shared/Filter/Filter.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+//import { useDispatch } from "react-redux";
+//import { filter } from "../../redux/Actions/actions";
+//import styles from "./Filter.module.css";
+
+const Filter = ({title, options, action}) => {
+    console.log(title + "// "+options);
+    //const dispatch = useDispatch()
+    const handleChangeFilter = (e) => {
+        //dispatch(action(e.target.value))
+        console.log(action + " value "+e.target.value)
+    };
+
+    return (
+        <div>
+            <label htmlFor="">{title}</label>
+                <select name="" id="" onChange={(e) => handleChangeFilter(e)}>
+                    { options?.map( (opt, index) => {
+                        return <option key={index} value={opt}>{opt}</option>
+                    })}
+                </select>
+            </div>
+    );
+};
+
+export default Filter;
