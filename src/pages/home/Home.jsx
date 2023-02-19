a9c908f1b636710818c1c5dd5696c085cc222233
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-19 11:39:42 -0500

    change landingpage for FoodCards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import LandingPage from "../../components/home/landingPage/LandingPage";
 import Carousel from "../../components/home/carousel/Carousel";
+import FoodCards from "../../components/home/foodCards/FoodCards";
 
 export default function Home() {
   return (
     <div>
       <Carousel />
-      <LandingPage />
+      <FoodCards />
     </div>
   );
 }
