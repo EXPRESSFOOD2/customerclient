b163099ccf1a3326a6c5a3f183c97899fa2ef612
constantin_m <cmitrokhin@applaudostudios.dev>
2023-02-19 05:17:44 -0300

    Adds Store Pagination

--- src/pages/detailMenu/DetailMenu.jsx
+++ src/pages/detailMenu/DetailMenu.jsx
@@ -12,7 +12,7 @@ const DetailMenu = () => {
     //name,description,price,recomend_first,stock,is_active,url_image
     return (
       <div>
-        <img src={url_image} alt={name}/>
+        <img src={url_image} alt={name} style={{width: "30%"}}/>
         <h1>{name}</h1>
         <h2>{description}</h2>
         <h3>$ {price}</h3>
--- src/pages/store/Store.jsx
+++ src/pages/store/Store.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import ProductCard from "../../components/store/Product/ProductCard";
 import style from "./Store.module.css";
 import { NavLink } from "react-router-dom";
+import Pagination from "../../shared/Pagination/Pagination";
 
 export default function Store() {
   const menu = useSelector((state) => state.fullMenu); // Hook de traer data del estado global
@@ -14,5 +15,22 @@ export default function Store() {
     );
   });
 
-  return <div className={style.container}>{products}</div>;
+  const [pagina, setPagina] = useState(1);
+  const [porPagina] = useState(9);
+  let maximo = Math.ceil(menu.length / porPagina);
+
+  return (
+    <>
+      <Pagination pagina={pagina} setPagina={setPagina} maximo={maximo} />
+      <div className={style.rows}>
+        {/* <div>
+          <p>opcion 1</p>
+          <p>opcion 2</p>
+        </div> */}
+        <div className={style.container}>
+          {products.slice(((pagina-1)*porPagina), (pagina)*porPagina)}
+        </div>
+      </div>
+    </>
+  )
 }
