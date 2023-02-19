b4eee89bf605c1d52aeca0188d0834a146665751
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-02-19 08:53:46 -0500

    add grid to menu items

--- src/components/store/Product/ProductCard.jsx
+++ src/components/store/Product/ProductCard.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import style from "./ProductCard.module.css";
 
-const ProductCard = ({menuItem}) => {
-    const {name, price, url_image} = menuItem;
+
+const ProductCard = ({ menuItem }) => {
+  const { name, price, url_image } = menuItem;
   return (
-    <div className={style.container}>
-      <img src={url_image} alt={name + "image"} />
-    <h2>{name}</h2>
-    <h3>$ {price}</h3>
+    <div className={style.card}>
+      {" "}
+  
+        <img src={url_image} alt={name + "image"} className={style.image} />
+     
+      <h2 className={style.title}>{name}</h2>
+      <span className={style.price}>${price}</span> 
     </div>
   );
 };
