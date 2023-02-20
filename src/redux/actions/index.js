2f4008944f25c1c80ed3cfbd090df2f9460c2f8b
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-20 11:31:18 -0500

    adding recommended menu in redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,19 +5,35 @@ export const GET_FULL_MENU = "GET_ALL_MENU";
 export const GET_FULL_INGREDIENTS = "GET_FULL_INGREDIENTS";
 export const GET_MENU_BY_ID = "GET_MENU_BY_ID";
 export const FILTER_MENU = "FILTER_MENU";
+export const GET_MENU_RECOMMENDED = 'GET_MENU_RECOMMENDED';
+export const ERROR = 'ERROR';
+
+
+export const getRecommendedMenu = () => {
+  return async (dispatch) => {
+    try {
+      const result = await axios.get(`localhost:3001/menu/get/recommended`)
+      let data = result.data;
+      dispatch({ type: GET_MENU_RECOMMENDED, payload: data })
+    } catch (error) {
+      dispatch({ type: ERROR, payload: error.response.data.error });
+    }
+
+  }
+}
 
 export const getFullMenu = () => {
   return async (dispatch) => {
     const result = await axios.get(`http://localhost:3001/menu/get`)
     let data = result.data;
-    dispatch({type: GET_FULL_MENU, payload: data})
+    dispatch({ type: GET_FULL_MENU, payload: data })
   }
 }
 export const getFullIngredients = () => {
   return async (dispatch) => {
     const result = await axios.get(`http://localhost:3001/ingredients/get`)
     let data = result.data;
-    dispatch({type: GET_FULL_INGREDIENTS, payload: data})
+    dispatch({ type: GET_FULL_INGREDIENTS, payload: data })
   }
 }
 
@@ -25,13 +41,13 @@ export const getMenuById = (id) => {
   return async (dispatch) => {
     const result = await axios.get(`http://localhost:3001/menu/get/${id}`)
     let data = result.data;
-    dispatch({type: GET_MENU_BY_ID, payload: data})
+    dispatch({ type: GET_MENU_BY_ID, payload: data })
   }
 }
-export const filterMenu = (category) => (dispatch)=> {
-   
-    dispatch({type: FILTER_MENU, payload: category})
-  }
+export const filterMenu = (category) => (dispatch) => {
+
+  dispatch({ type: FILTER_MENU, payload: category })
+}
 
 
 
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,28 +1,38 @@
-import { GET_FULL_MENU, GET_MENU_BY_ID, GET_FULL_INGREDIENTS, FILTER_MENU } from "../actions/index"
+import { GET_FULL_MENU, GET_MENU_BY_ID, GET_FULL_INGREDIENTS, FILTER_MENU, GET_MENU_RECOMMENDED, ERROR } from "../actions/index"
 
 // @initialState == estado inicial del REDUCER
 const initialState = {
     fullMenu: [],
     filteredMenu: [],
     allIngredients: [],
+    recommendedMenu: [],
     detailMenu: {},
     isFiltered: false,
+    error: ''
 };
 
-  // @state   = Estado Actual
-  // @action  = Accion despachada
+// @state   = Estado Actual
+// @action  = Accion despachada
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
+        // Menu
         case GET_FULL_MENU:
             return { ...state, fullMenu: action.payload }
         case GET_MENU_BY_ID:
             return { ...state, detailMenu: action.payload }
+        case GET_MENU_RECOMMENDED:
+            return { ...state, recommendedMenu: action.payload }
+        // ingredientes
         case GET_FULL_INGREDIENTS:
             return { ...state, allIngredients: action.payload }
         case FILTER_MENU:
-            if(state.isFiltered){
-                return  { ...state, filteredMenu: state.filteredMenu.filter() }
+            if (state.isFiltered) {
+                return { ...state, filteredMenu: state.filteredMenu.filter() }
             }
+            return { ...state }
+
+        case ERROR:
+            return { ...state, error: action.payload }
         default:
             return { ...state };
     }
