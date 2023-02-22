df787c68ce55e9ddbe03e2c35acfa4ffdfd8e517
Pedro Munoz <Pedrojose0272@gmail.com>
2023-02-22 12:50:30 -0500

    Add login style

--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -5,10 +5,21 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { NavLink, useLocation } from "react-router-dom";
 import { getFullMenu, getFullIngredients } from "../../redux/actions/index";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Navbar() {
   /* Siempre que está el Navbar se carga el State */
   const location = useLocation().pathname.split("/").at(1);
+
+  const queryParams = new URLSearchParams(window.location.search);
+
+  const user = queryParams.get("user");
+
+  if (user && !localStorage.getItem("user")) {
+    localStorage.setItem("user", user);
+  }
+
+  const { photo, userName } = JSON.parse(localStorage.getItem("user")) || {};
 
   const dispatch = useDispatch(); // Dispachador de Redux
   useEffect(() => {
@@ -26,17 +37,34 @@ export default function Navbar() {
           <img src={logo} className={style.logo} alt="" />
         </NavLink>
       </div>
+      <div className={style.userContainer}>
+        {photo && <img src={photo} alt="" />}
+        {userName && <span>{userName}</span>}
+      </div>
       <div className={style.linkContainer}>
         <NavLink to="/store">
           <p className={location === "store" ? style.current : ""}>Tienda</p>
         </NavLink>
-        <NavLink to="/login">
-          <p className={location === "login" ? style.current : ""}>Login</p>
-        </NavLink>
-        <NavLink to={"/register"}>
-          <p className={location === "register" ? style.current : ""}>
-            Registarte
-          </p>
+        {!userName && (
+          <NavLink to="/login">
+            <p className={location === "login" ? style.current : ""}>Login</p>
+          </NavLink>
+        )}
+        {!userName && (
+          <NavLink to={"/register"}>
+            <p className={location === "register" ? style.current : ""}>
+              Registarte
+            </p>
+          </NavLink>
+        )}
+        {userName && (
+          <NavLink to="/" onClick={() => localStorage.removeItem("user")}>
+            <p className={location === "login" ? style.current : ""}>Salir</p>
+          </NavLink>
+        )}
+
+        <NavLink to={"/"} className={style.center}>
+          <ShoppingCartIcon sx={{ fontSize: "25px" }} />
         </NavLink>
       </div>
     </div>
