aa6a35af88e029b0d7f7bb316ac249172a141837
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-02-24 17:00:10 -0500

    Corrigiendo Redux y {}en case donde se declaran variables

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -5,38 +5,36 @@ export const GET_FULL_MENU = "GET_ALL_MENU";
 export const GET_FULL_INGREDIENTS = "GET_FULL_INGREDIENTS";
 export const GET_MENU_BY_ID = "GET_MENU_BY_ID";
 export const FILTER_MENU = "FILTER_MENU";
-export const GET_MENU_RECOMMENDED = 'GET_MENU_RECOMMENDED';
-export const ERROR = 'ERROR';
-export const RESET_FILTER = 'RESET_FILTER';
-export const CHANGE_CART_COUNT = 'CHANGE_CART_COUNT'
+export const GET_MENU_RECOMMENDED = "GET_MENU_RECOMMENDED";
+export const ERROR = "ERROR";
+export const RESET_FILTER = "RESET_FILTER";
+export const CHANGE_CART_COUNT = "CHANGE_CART_COUNT";
+export const CHANGE_CART_TOTAL = "CHANGE_CART_TOTAL";
 
 //*
 export const GET_TAGS = "GET_TAGS";
 
 export const processFilterAction = (filters) => async (dispatch) => {
-    //dispatch(getFullMenu());    Cuando como demora en ejecutarse... sobreescribe el filtrado
-    dispatch(resetFilter());
-    dispatch({type: FILTER_MENU, payload: filters});
-
-}
+  //dispatch(getFullMenu());    Cuando como demora en ejecutarse... sobreescribe el filtrado
+  dispatch(resetFilter());
+  dispatch({ type: FILTER_MENU, payload: filters });
+};
 export const getTagsAction = () => {
   return async (dispatch) => {
     try {
       const result = await axios.get(`http://localhost:3001/tags/get`);
       let data = result.data;
-      dispatch({type: GET_TAGS, payload: data})
+      dispatch({ type: GET_TAGS, payload: data });
     } catch (error) {
       dispatch({ type: ERROR, payload: error.response.data.error });
     }
-  }
-}
-
+  };
+};
 
 export const getRecommendedMenu = () => {
   return async (dispatch) => {
-    try {  
-
-      const result = await axios.get(`/menu/get/recommended`)
+    try {
+      const result = await axios.get(`/menu/get/recommended`);
 
       let data = result.data;
       dispatch({ type: GET_MENU_RECOMMENDED, payload: data });
@@ -51,7 +49,7 @@ export const changeCartCount = (op) => {
     dispatch({ type: CHANGE_CART_COUNT, payload: op });
   };
 };
-export const changeCartTotal = (obj) => {  
+export const changeCartTotal = (obj) => {
   return async (dispatch) => {
     dispatch({ type: CHANGE_CART_TOTAL, payload: obj });
   };
@@ -59,17 +57,15 @@ export const changeCartTotal = (obj) => {
 
 export const getFullMenu = () => {
   return async (dispatch) => {
-
-    const result = await axios.get(`http://localhost:3001/menu/get`)
+    const result = await axios.get(`http://localhost:3001/menu/get`);
     let data = result.data;
-    dispatch({ type: GET_FULL_MENU, payload: data })
-  }
-}
+    dispatch({ type: GET_FULL_MENU, payload: data });
+  };
+};
 
 export const getFullIngredients = () => {
   return async (dispatch) => {
-
-    const result = await axios.get(`/ingredients/get`)
+    const result = await axios.get(`/ingredients/get`);
 
     let data = result.data;
     dispatch({ type: GET_FULL_INGREDIENTS, payload: data });
@@ -78,39 +74,31 @@ export const getFullIngredients = () => {
 
 export const getMenuById = (id) => {
   return async (dispatch) => {
-    const result = await axios.get(`/menu/get/${id}`)
+    const result = await axios.get(`/menu/get/${id}`);
 
     let data = result.data;
     dispatch({ type: GET_MENU_BY_ID, payload: data });
   };
 };
 export const filterMenu = (category) => (dispatch) => {
-
-  dispatch({ type: FILTER_MENU, payload: category })
-}
+  dispatch({ type: FILTER_MENU, payload: category });
+};
 export const resetFilter = () => (dispatch) => {
-
-  dispatch({ type: RESET_FILTER })
-}
-
+  dispatch({ type: RESET_FILTER });
+};
 
 export const getImageUrl = (imageStr, imageFn) => {
-  return async () => {
+  return async (dispatch) => {
     try {
-
-      let result = await axios.post("/processImage/post", { imageStr: imageStr })
-      imageFn(result.data)
+      let result = await axios.post("/processImage/post", {
+        imageStr: imageStr,
+      });
+      imageFn(result.data);
 
       //! ?! manejar Success && Error
       return result;
     } catch (error) {
-
       dispatch({ type: ERROR, payload: error.response.data.error });
-
-
-
     }
   };
 };
-
-
