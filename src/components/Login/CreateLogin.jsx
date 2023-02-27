b2c9548286b68b70ed48296766d4b6fc1ef22010
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-02-26 20:22:06 -0500

    Update CreateLogin.jsx

--- src/components/Login/CreateLogin.jsx
+++ src/components/Login/CreateLogin.jsx
@@ -12,52 +12,7 @@ const CreateLogin = () => {
               {/* <div className={style.prev}> */}
               <img src={img} alt="not found" className={style.image} />
               <p>Por favor logueate para hacer tu pago</p>
-              {/* </div> */}
-              {/* <form action="" className={style.form} onSubmit={formik.handleSubmit}>
-
-          <div className={style.col}>
-            <label htmlFor="email">E-mail</label>
-            <input
-              type="text"
-              className={formik.errors.email ? style.errorInput : ""}
-              id="email"
-              name="email"
-              placeholder="E-mail"
-              {...formik.getFieldProps("email")}
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <label className={style.errorText}>{formik.errors.email}</label>
-            ) : null}
-          </div>
-          <div className={style.col}>
-            <label htmlFor="password">Contraseña</label>
-            <input
-              type="password"
-              className={formik.errors.password ? style.errorInput : ""}
-              id="password"
-              name="password"
-              placeholder="Contraseña"
-              {...formik.getFieldProps("password")}
-            />
-            {formik.touched.password && formik.errors.password ? (
-              <label className={style.errorText}>
-                {formik.errors.password}
-              </label>
-            ) : null}
-            <a href="#">olvide mi contraseña</a>
-          </div>
-          <div className={style.logins}>
-            <button type="submit">iniciar sesión</button>
-          </div>
-          <div className={style.checkbox}>
-            <input type="checkbox" name="" id="" value="rememberMe" />
-            <label htmlFor="">Recuerdame</label>
-          </div>
-        </form> */}
-              {/* <div className={style.question}>
-          <p>¿primera vez en Space Food? </p>{" "}
-          <a href="/register"> ¡Registrate! </a>
-        </div> */}
+            
               <div className={style.loginsGo}>
                   <img src="https://cdn-icons-png.flaticon.com/512/2702/2702602.png" alt="" />
                   <button
