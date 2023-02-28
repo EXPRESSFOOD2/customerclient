d2a8d98dc462199b08a771870d030352f3daf4f9
Davidvn95 <davidvergaraok@gmail.com>
2023-02-28 16:34:46 -0500

    Termonando Order Page

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store/index"
 import axios from "axios"
 
-// axios.defaults.baseURL ="https://apiexpressfood.up.railway.app/";
+// axios.defaults.baseURL ="https://apiexpressfood.up.railway.app";
 
 axios.defaults.baseURL ="http://localhost:3002";
 
