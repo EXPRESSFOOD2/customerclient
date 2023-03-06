c0f78bbe9be04068edcc4e8bea8eb2d78b91cbd1
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-03-05 22:54:14 -0500

    Se muestra puntuacion en el detalle del menu

--- src/components/reviews/cardsContainer/CardsContainer.jsx
+++ src/components/reviews/cardsContainer/CardsContainer.jsx
@@ -5,8 +5,7 @@ import React, { useEffect, useState } from "react";
 import style from "./cards.module.css";
 import axios from "axios";
 import Alert from "../../../shared/Alert/Alert";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 export default function CardsContainer({
   id,
@@ -17,9 +16,10 @@ export default function CardsContainer({
 }) {
   const [rating, setRating] = useState({});
   const [buttonDisabled, setButtonDisabled] = useState(false);
-  const [alertVisibleBeforeReview, setAlertVisibleBeforeReview] = useState(false);
+  const [alertVisibleBeforeReview, setAlertVisibleBeforeReview] =
+    useState(false);
   const [alertVisibleAfterReview, setAlertVisibleAfterReview] = useState(true);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   async function sendReview(item) {
     await axios.post("/review/post", {
@@ -31,23 +31,18 @@ export default function CardsContainer({
 
   async function handledReview() {
     if (MenuItems.length !== Object.values(rating).length) {
- 
-      setAlertVisibleBeforeReview(true)
- 
+      setAlertVisibleBeforeReview(true);
+    } else {
+      setButtonDisabled(true);
+
+      navigate("/store");
+
+      try {
+        for (const item in rating) {
+          await sendReview(item);
+        }
+      } catch (error) {}
     }
-   else{
-
-    setButtonDisabled(true);
-
-      navigate('/store')
-  
-
-    try {
-      for (const item in rating) {
-        await sendReview(item);
-      }
-    } catch (error) {}
-  }
     /*
     Enviar : rating, tittle,comment,ordersMenuId,MenuItemId
     */
@@ -103,16 +98,19 @@ export default function CardsContainer({
         disabled={buttonDisabled}
       >
         Envia tu calificacion
-      </button><div className={style.divAlert}>
-     {alertVisibleBeforeReview && <Alert 
- title="Calificacion en curso!"
- message="Por favor puntua todos los productos de tu orden"
- type="danger"
-stateAlert ={alertVisibleBeforeReview}
-setStateAlert ={setAlertVisibleBeforeReview}
-setStatebutton={setButtonDisabled}
-
-/>} </div>
+      </button>
+      <div className={style.divAlert}>
+        {alertVisibleBeforeReview && (
+          <Alert
+            title="Calificacion en curso!"
+            message="Por favor puntua todos los productos de tu orden"
+            type="danger"
+            stateAlert={alertVisibleBeforeReview}
+            setStateAlert={setAlertVisibleBeforeReview}
+            setStatebutton={setButtonDisabled}
+          />
+        )}{" "}
+      </div>
     </div>
   ) : (
     <div className={style.container}>
@@ -144,15 +142,17 @@ setStatebutton={setButtonDisabled}
         })}
       </div>
       <div className={style.divAlert}>
-     {alertVisibleAfterReview && <Alert 
- title="Orden con Reseña"
- message="Parece qua ya puntuaste esta orden, compra mas para seguir puntuando nuestros productos!"
- type="success"
-stateAlert ={alertVisibleAfterReview}
-setStateAlert ={setAlertVisibleAfterReview}
-setStatebutton={setButtonDisabled}
-
-/>} </div>
+        {alertVisibleAfterReview && (
+          <Alert
+            title="Orden con Reseña"
+            message="Parece qua ya puntuaste esta orden, compra mas para seguir puntuando nuestros productos!"
+            type="success"
+            stateAlert={alertVisibleAfterReview}
+            setStateAlert={setAlertVisibleAfterReview}
+            setStatebutton={setButtonDisabled}
+          />
+        )}{" "}
+      </div>
     </div>
   );
 }
