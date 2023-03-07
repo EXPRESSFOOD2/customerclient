8c455c5471e141d125e803263f8516a135ee6694
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-03-07 09:53:35 -0500

    fix ordersPage bug

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -7,63 +7,62 @@ import { saveCart } from "../../redux/actions";
 import { Link } from "react-router-dom";
 
 const OrderPage = () => {
-    const userdata = JSON.parse(localStorage.getItem("user"));
-    const myOrders = useSelector((state) => state.cartSaved);
-    const dispatch = useDispatch();
-    const [isOpen, setIsOpen] = useState(false);
+  const userdata = JSON.parse(localStorage.getItem("user"));
+  const myObject = { email: userdata.email };
 
-    useEffect(() => {
-        setTimeout(()=>{
-            dispatch(saveCart(userdata));
-        }, 30000)
-      
-    }, [dispatch, myOrders]);
+  const myOrders = useSelector((state) => state.cartSaved);
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleOrder = () => {
-        setIsOpen(isOpen ? false : true);
-    };
+  useEffect(() => {
+    userdata && dispatch(saveCart(myObject));
+  }, []);
 
-    return (
-        <div className={styles.page}>
-            <div className={styles.container}>
-                {myOrders.length ? (
-                    myOrders.map((item) => (
-                        <div key={item.id} className={styles.subContainer}>
-                            <div className={styles.data}>
-                                <span>{`Pedido: ${item.code}`}</span>
-                                <span>{`Estado: ${item.status}`}</span>
-                                <span>{`Total compra: ${item.total}`}</span>
-                                <img
-                                    className={isOpen ? styles.icoOpen : styles.icoClose}
-                                    onClick={handleOrder}
-                                    src="https://cdn-icons-png.flaticon.com/512/9861/9861174.png"
-                                    alt=""
-                                />
-                            </div>
-                            {isOpen && (
-                                <div className={styles.items}>
-                                    {item.MenuItems?.map((menu, i) => (
-                                        <OrdersComponent
-                                            key={i}
-                                            image={menu.url_image}
-                                            name={menu.name}
-                                        />
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    ))
-                ) : (
-                    <div className={styles.message}>
-                        <h2>¿Aún no haces tu primer compra hoy... 🫣?</h2>
-                        <Link to="/store">
-                            <span>Haz tu compra ahora 😎</span>
-                        </Link>
-                    </div>
-                )}
+  const handleOrder = () => {
+    setIsOpen(isOpen ? false : true);
+  };
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.container}>
+        {myOrders.length ? (
+          myOrders.map((item) => (
+            <div key={item.id} className={styles.subContainer}>
+              <div className={styles.data}>
+                <span>{`Pedido: ${item.code}`}</span>
+                <span>{`Estado: ${item.status}`}</span>
+                <span>{`Total compra: ${item.total}`}</span>
+                <img
+                  className={isOpen ? styles.icoOpen : styles.icoClose}
+                  onClick={handleOrder}
+                  src="https://cdn-icons-png.flaticon.com/512/9861/9861174.png"
+                  alt=""
+                />
+              </div>
+              {isOpen && (
+                <div className={styles.items}>
+                  {item.MenuItems?.map((menu, i) => (
+                    <OrdersComponent
+                      key={i}
+                      image={menu.url_image}
+                      name={menu.name}
+                    />
+                  ))}
+                </div>
+              )}
             </div>
-        </div>
-    );
+          ))
+        ) : (
+          <div className={styles.message}>
+            <h2>¿Aún no haces tu primer compra hoy... 🫣?</h2>
+            <Link to="/store">
+              <span>Haz tu compra ahora 😎</span>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OrderPage;
