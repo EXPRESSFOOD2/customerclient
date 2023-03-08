b77a05e53401c8b2022c13f27add37cb20eec047
constantin_m <cmitrokhin@applaudostudios.dev>
2023-03-08 14:21:14 -0300

    un poco de magia

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -10,88 +10,6 @@ import { socket } from "../../App";
 
 
 const OrderPage = () => {
-<<<<<<< HEAD
-    const user = JSON.parse(localStorage.getItem("user"));
-    const myOrders = useSelector((state) => state.cartSaved);
-    const dispatch = useDispatch();
-    const [orderDB , setOrderDB] = useState([])
-    const [reload,setReload] = useState(false)
-    const [isOpen, setIsOpen] = useState(false);
-    let renderMyOrders = myOrders
-
-    
-    useEffect(() => {
-
-            // socket.emit('updateOrders',({"email":user.email}))
-            
-
-        dispatch(saveCart({ email: user.email }));
-        
-        
-    }, []);
-    
-    useEffect(() => {
-        const socket = io.connect('http://localhost:3002')
-        socket.on('sendOrders',(data)=>{
-            console.log(data);
-            dispatch(saveCart({ email: user.email }));
-            // renderMyOrders= myOrders
-          })
-      setInterval(() => {
-        // console.log('hola');
-        socket.emit('updateOrders',({"email":user.email}))
-      }, 5000);  
-      return () => {
-        clearInterval()
-        socket.disconnect()
-      }
-    }, [myOrders])
-    
-   
-    const handleOrder = () => {
-        setIsOpen(isOpen ? false : true);
-    };
-
-    return (
-        <div className={styles.page}>
-            <div className={styles.container}>
-            {console.log(orderDB)}
-                {myOrders.length ? (
-                    myOrders.map((item) => (
-                        <div key={item.id} className={styles.subContainer}>
-                            <div className={styles.data}>
-                                <span>{`Pedido: ${item.code}`}</span>
-                                <span>{`Estdo: ${item.status}`}</span>
-                                <span>{`Total compra: ${item.total}`}</span>
-                                <img
-                                    className={isOpen ? styles.icoOpen : styles.icoClose}
-                                    onClick={handleOrder}
-                                    src="https://cdn-icons-png.flaticon.com/512/9861/9861174.png"
-                                    alt=""
-                                />
-                            </div>
-                            {isOpen && (
-                                <div className={styles.items}>
-                                    {item.MenuItems?.map((menu, i) => (
-                                        <OrdersComponent
-                                            key={i}
-                                            image={menu.url_image}
-                                            name={menu.name}
-                                        />
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    ))
-                ) : (
-                    <div className={styles.message}>
-                        <h2>¿Aún no haces tu primer compra hoy... 🫣?</h2>
-                        <Link to="/store">
-                            <span>Haz tu compra ahora 😎</span>
-                        </Link>
-                    </div>
-                )}
-=======
   const userdata = JSON.parse(localStorage.getItem("user"));
   const myObject = { email: userdata.email };
 
@@ -135,7 +53,6 @@ const OrderPage = () => {
                   ))}
                 </div>
               )}
->>>>>>> e8438db520c54d4de0a5b67186649c65080d8a85
             </div>
           ))
         ) : (
