50709da78ce74e10ca68c87fc3335e685a31d57f
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-03-09 05:07:39 +0000

    dsdsd

--- src/pages/reviews/Reviews.jsx
+++ src/pages/reviews/Reviews.jsx
@@ -8,7 +8,6 @@ import CardsContainer from "../../components/reviews/cardsContainer/CardsContain
 import CreateLogin from "../../components/Login/CreateLogin";
 
 import style from "./Reviews.module.css";
-const mipedido = { id: 22, total: 36, client_data: { email: "gibsonavilan@gmail.com" }, code: "A021", status: "Entregada", payment_data: { code: "22", collection_id: "1313123075", collection_status: "approved", payment_id: "1313123075", status: "approved", external_reference: "null", payment_type: "credit_card", merchant_order_id: "8099873339", preference_id: "463377658-f65a6b95-3718-4278-a1cc-eb483bab8e4e", site_id: "MLA", processing_mode: "aggregator", merchant_account_id: "null" }, createdAt: "2023-03-09T04:38:30.666Z", updatedAt: "2023-03-09T04:39:37.599Z", deletedAt: null, store_id: "f3bc0474-620c-429d-a46c-df2460c7725a", MenuItems: [{ name: "Canasto Floral Aniversario", url_image: "https://res.cloudinary.com/group7foodexpress/image/upload/v1678150203/gxlimir3mkwvxxsjns1t.jpg", OrdersMenu: { id: 85, quantity: 1, unitPrice: 36, OrderId: 22, MenuItemId: 2 } }] }
 export default function reviews() {
   const { orderId } = useParams();
   const [pedido, setPedido] = useState();
@@ -20,24 +19,24 @@ export default function reviews() {
 
       if (Object.keys(userEmail).length) {
         async function data() {
-          // try {
+          try {
           const response = await axios.post(
             `/orders/get/${orderId}`,
             userEmail
           );
-          if (Object.keys.length(response.data)) setPedido(response.data);
+         setPedido(response.data);
 
-          setPedido(mipedido)
-          // } catch (error) {
-          //   return error.message;
-          // }
+         
+          } catch (error) {
+            return error.message;
+          }
         }
         data();
       }
     }, []);
   }
   if (dataClient !== null) {
-    // if (pedido) {
+    if (pedido) {
     return (
       <div className={style.container}>
         <div className={style.cart}>
@@ -45,13 +44,13 @@ export default function reviews() {
         </div>
       </div>
     );
-    // } else {
-    //   return (
-    //     <div className={style.container}>
-    //       <h1>Oops no tenemos esta orden asociada a tu cuenta! ☹</h1>
-    //     </div>
-    //   );
-    // }
+    } else {
+      return (
+        <div className={style.container}>
+          <h1>Oops no tenemos esta orden asociada a tu cuenta! ☹</h1>
+        </div>
+      );
+    }
   } else {
     return (
       <div>
