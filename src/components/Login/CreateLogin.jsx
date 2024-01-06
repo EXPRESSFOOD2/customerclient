46f4103c206a0bd2604ce2eb070dc427992fd6c4
siriusg2 <gibsonavilan@gmail.com>
2024-01-06 10:56:01 -0500

    migrate to vite complete

--- src/components/Login/CreateLogin.jsx
+++ src/components/Login/CreateLogin.jsx
@@ -1,35 +1,53 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
-import React from "react";
+
 import { useLocation } from "react-router-dom";
 import style from "./CreateLogin.module.css";
 import img from "./image/logoSinFondo.png";
-
+const {
+  VITE_GOOGLE_REDIRECT_URL_LOGINCOMPONENT_LOCAL,
+  VITE_GOOGLE_REDIRECT_URL_LOGINCOMPONENT_DEPLOY,
+} = import.meta.env;
 const CreateLogin = () => {
-    const { pathname } = useLocation()
+  const { pathname } = useLocation();
   return (
-      <div className={pathname === "/login"? style.superDivInLogin :style.superDiv}>
-          <div className={pathname === "/login"? style.createLoginInLogin :style.createLogin}>
-          
-              <img src={img} alt="not found" className={style.image} />
-              <p>Por favor logueate para hacer tu pago</p>
- 
-              <div className={style.loginsGo}>
-                  <img src="https://cdn-icons-png.flaticon.com/512/2702/2702602.png" alt="" />
-                  <button
-                      type="submit"
-                      onClick={() =>
-                          (window.location.href = `${process.env.REACT_APP_GOOGLE_REDIRECT_URL_LOGINCOMPONENT_LOCAL || process.env.REACT_APP_GOOGLE_REDIRECT_URL_LOGINCOMPONENT_DEPLOY}` )
-                      }>
-                      Google
-                  </button>
-              </div>
-              <div className={style.loginsFa}>
-                  <img src="https://cdn-icons-png.flaticon.com/128/747/747543.png" alt="" />
-                  <button>Facebook</button>
-              </div>
-          </div>
+    <div
+      className={pathname === "/login" ? style.superDivInLogin : style.superDiv}
+    >
+      <div
+        className={
+          pathname === "/login" ? style.createLoginInLogin : style.createLogin
+        }
+      >
+        <img src={img} alt="not found" className={style.image} />
+        <p>Por favor logueate para hacer tu pago</p>
+
+        <div className={style.loginsGo}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2702/2702602.png"
+            alt=""
+          />
+          <button
+            type="submit"
+            onClick={() =>
+              (window.location.href = `${
+                VITE_GOOGLE_REDIRECT_URL_LOGINCOMPONENT_LOCAL ||
+                VITE_GOOGLE_REDIRECT_URL_LOGINCOMPONENT_DEPLOY
+              }`)
+            }
+          >
+            Google
+          </button>
+        </div>
+        <div className={style.loginsFa}>
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/747/747543.png"
+            alt=""
+          />
+          <button>Facebook</button>
+        </div>
       </div>
+    </div>
   );
 };
 
